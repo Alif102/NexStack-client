b11ca4551d615cfc809f147db3f912a8e82e399b
Alif Ahmed <alifahmed102@gmail.com>
2025-08-11 23:57:20 +0600

    Category update

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Outlet, useLocation } from 'react-router-dom'
 import './App.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import toast, { Toaster } from 'react-hot-toast';
+import  { Toaster } from 'react-hot-toast';
 import { useEffect } from 'react';
 import fetchUserDetails from './utils/fetchUserDetails';
 import { setUserDetails } from './store/userSlice';
@@ -10,10 +10,8 @@ import { setAllCategory,setAllSubCategory,setLoadingCategory } from './store/pro
 import { useDispatch } from 'react-redux';
 import Axios from './utils/Axios';
 import SummaryApi from './common/SummaryApi';
-import { handleAddItemCart } from './store/cartProduct'
 import GlobalProvider from './provider/GlobalProvider';
-import { FaCartShopping } from "react-icons/fa6";
-import CartMobileLink from './components/CartMobile';
+
 
 function App() {
   const dispatch = useDispatch()
@@ -76,11 +74,7 @@ function App() {
       </main>
       <Footer/>
       <Toaster/>
-      {
-        location.pathname !== '/checkout' && (
-          <CartMobileLink/>
-        )
-      }
+     
     </GlobalProvider>
   )
 }
