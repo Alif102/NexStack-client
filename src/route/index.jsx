47dfc0c34897f6bf8833da93eac87c16ddf76938
Alif Ahmed <alifahmed102@gmail.com>
2025-08-21 21:13:10 +0600

    product details page

---
+++ src/route/index.jsx
@@ -0,0 +1,115 @@
+import { createBrowserRouter } from "react-router-dom";
+import App from "../App";
+import Home from "../pages/Home";
+import SearchPage from "../pages/SearchPage";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import ForgotPassword from "../pages/ForgotPassword";
+import OtpVerification from "../pages/OtpVerification";
+import ResetPassword from "../pages/ResetPassword";
+import UserMenuMobile from "../pages/UserMenuMobile";
+import Dashboard from "../layouts/Dashboard";
+import Profile from "../pages/Profile";
+
+import CategoryPage from "../pages/CategoryPage";
+
+// import ProductAdmin from "../pages/ProductAdmin";
+
+import Cancel from "../pages/Cancel";
+import AdminPermision from "../layouts/AdminPermission";
+import SubCategoryPage from "../pages/SubCategoryPage";
+import ProductAdmin from "../pages/ProductAdmin";
+import UploadProduct from "../pages/UploadProduct";
+import ProductListPage from "../pages/ProductListPage";
+import ProductDisplayPage from "../pages/ProductDisplayPage";
+
+const router = createBrowserRouter([
+    {
+        path : "/",
+        element : <App/>,
+        children : [
+            {
+                path : "",
+                element : <Home/>
+            },
+            {
+                path : "search",
+                element : <SearchPage/>
+            },
+            {
+                path : 'login',
+                element : <Login/>
+            },
+            {
+                path : "register",
+                element : <Register/>
+            },
+            {
+                path : "forgot-password",
+                element : <ForgotPassword/>
+            },
+            {
+                path : "verification-otp",
+                element : <OtpVerification/>
+            },
+            {
+                path : "reset-password",
+                element : <ResetPassword/>
+            },
+            {
+                path : "user",
+                element : <UserMenuMobile/>
+            },
+            {
+                path : "dashboard",
+                element : <Dashboard/>,
+                children : [
+                    {
+                        path : "profile",
+                        element : <Profile/>
+                    },
+                  
+                   
+                    {
+                        path : 'category',
+                        element : <AdminPermision><CategoryPage/></AdminPermision>
+                    },
+                    {
+                        path : "subcategory",
+                        element : <AdminPermision><SubCategoryPage/></AdminPermision>
+                    },
+                    {
+                        path : 'upload-product',
+                        element : <AdminPermision><UploadProduct/></AdminPermision>
+                    },
+                    {
+                        path : 'product',
+                        element : <AdminPermision><ProductAdmin/></AdminPermision>
+                    }
+                ]
+            },
+            {
+                path : ":category",
+                children : [
+                    {
+                        path : ":subCategory",
+                        element : <ProductListPage/>
+                    }
+                ]
+            },
+            {
+                path : "product/:product",
+                element : <ProductDisplayPage/>
+            },
+          
+           
+          
+            {
+                path : 'cancel',
+                element : <Cancel/>
+            }
+        ]
+    }
+])
+
+export default router
